840f458ac05356c502ce23b8dc210d10682dd096
Sam Rabin <samrabin@ucar.edu>
2025-03-27 11:47:25 -0600

    Use container testalpine-01.

--- doc_builder/build_commands.py
+++ doc_builder/build_commands.py
@@ -7,7 +7,7 @@ import pathlib
 from doc_builder import sys_utils
 
 # The Docker image used to build documentation via Docker
-DOCKER_IMAGE = "ctsm-docs-test10"
+DOCKER_IMAGE = "ctsm-docs-testalpine-01"
 
 # The path in Docker's filesystem where the user's home directory is mounted
 _DOCKER_HOME = "/home/user/mounted_home"
