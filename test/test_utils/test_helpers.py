493dc33b07a8574e8692454a4f4a63d37e527d82
Sam Rabin <samrabin@ucar.edu>
2025-05-23 15:15:45 -0600

    check_call_suppress_output(): Don't suppress stderr.

--- test/test_utils/test_helpers.py
+++ test/test_utils/test_helpers.py
@@ -6,4 +6,4 @@ import subprocess
 def check_call_suppress_output(args):
     """Make a check_call call with the given args, suppressing all output"""
     with open(os.devnull, 'w') as devnull:
-        subprocess.check_call(args, stdout=devnull, stderr=devnull)
+        subprocess.check_call(args, stdout=devnull)
