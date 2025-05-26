a14bc28c610b447ad36fd06f2be613e937ca9e04
Sam Rabin <samrabin@ucar.edu>
2025-05-26 14:50:30 -0600

    Add tests to ~fully cover sys_utils.py.

--- test/test_utils/git_helpers.py
+++ test/test_utils/git_helpers.py
@@ -8,12 +8,20 @@ def make_git_repo():
     """Turn the current directory into an empty git repository"""
     check_call_suppress_output(" ".join(['git', 'init']))
 
-def add_git_commit():
-    """Add a git commit in the current directory"""
-    with open('README', 'a') as myfile:
+def add_file(filename=None):
+    """Add a file and write some text to it"""
+    if not filename:
+        filename = "README"
+    with open(filename, 'a') as myfile:
         myfile.write('more info')
-    check_call_suppress_output(" ".join(['git', 'add', 'README']))
+    return filename
+
+def add_git_commit(filename=None):
+    """Add a git commit in the current directory"""
+    filename = add_file(filename=filename)
+    check_call_suppress_output(" ".join(['git', 'add', filename]))
     check_call_suppress_output("git commit -m 'my commit message'")
+    return filename
 
 def checkout_git_branch(branchname):
     """Checkout a new branch in the current directory"""
