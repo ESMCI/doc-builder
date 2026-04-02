bba89e98c0fcff9123bfb195eb69bd6bf3e5ad7a
Sam Rabin <sam.rabin@gmail.com>
2026-04-02 14:53:36 -0600

    test_unit_fix_command_for_missing_subids: Reduce duplication and avoid magic strings.

--- test/test_unit_fix_command_for_missing_subids.py
+++ test/test_unit_fix_command_for_missing_subids.py
@@ -9,107 +9,71 @@ from unittest.mock import patch
 
 # pylint: disable=import-error
 from doc_builder.build_docs import _fix_command_for_missing_subids, run_build_command
+from doc_builder.build_commands import DEFAULT_IMAGE
 
 # Allow names that pylint doesn't like, because otherwise I find it hard
 # to make readable unit test names
 # pylint: disable=invalid-name
 
+# Build commands to be tested
+_BASE_VOLUME_STR = "/path/to/repo:/home/user/mounted_home:U"
+_FIXED_VOLUME_STR = "/path/to/repo:/home/user/mounted_home"
+_BASE_USER_STR = "37083:1000"
+_FIXED_USER_STR = "0:0"
+_BASE_COMMAND = [
+    "podman",
+    "run",
+    "--name",
+    "foo",
+    "--user",
+    _BASE_USER_STR,
+    "-v",
+    _BASE_VOLUME_STR,
+    "--workdir",
+    "/home/user/mounted_home/doc",
+    "-t",
+    "--rm",
+    DEFAULT_IMAGE,
+    "make",
+    "html",
+]
+_FIXED_COMMAND = []
+for x in _BASE_COMMAND:
+    if x == _BASE_VOLUME_STR:
+        _FIXED_COMMAND.append(_FIXED_VOLUME_STR)
+    elif x == _BASE_USER_STR:
+        _FIXED_COMMAND.append(_FIXED_USER_STR)
+    else:
+        _FIXED_COMMAND.append(x)
+
 
 class TestFixCommandForMissingSubids(unittest.TestCase):
     """Test the _fix_command_for_missing_subids function"""
 
+    def setUp(self):
+        self.result = _fix_command_for_missing_subids(list(_BASE_COMMAND))
+
     def test_removes_U_suffix_from_mount(self):
         """The :U suffix should be removed from volume mount arguments"""
-        command = [
-            "podman",
-            "run",
-            "--name",
-            "foo",
-            "--user",
-            "37083:1000",
-            "-v",
-            "/path/to/repo:/home/user/mounted_home:U",
-            "--workdir",
-            "/home/user/mounted_home/doc",
-            "-t",
-            "--rm",
-            "ghcr.io/escomp/ctsm/ctsm-docs:v1.0.1",
-            "make",
-            "html",
-        ]
-        result = _fix_command_for_missing_subids(command)
-        self.assertIn("/path/to/repo:/home/user/mounted_home", result)
-        self.assertNotIn("/path/to/repo:/home/user/mounted_home:U", result)
+        self.assertIn("/path/to/repo:/home/user/mounted_home", self.result)
+        self.assertNotIn("/path/to/repo:/home/user/mounted_home:U", self.result)
 
     def test_changes_user_to_root(self):
         """--user should be changed to 0:0"""
-        command = [
-            "podman",
-            "run",
-            "--name",
-            "foo",
-            "--user",
-            "37083:1000",
-            "-v",
-            "/path/to/repo:/home/user/mounted_home:U",
-            "ghcr.io/escomp/ctsm/ctsm-docs:v1.0.1",
-            "make",
-            "html",
-        ]
-        result = _fix_command_for_missing_subids(command)
-        user_idx = result.index("--user")
-        self.assertEqual(result[user_idx + 1], "0:0")
+        user_idx = self.result.index("--user")
+        self.assertEqual(self.result[user_idx + 1], _FIXED_USER_STR)
 
     def test_preserves_other_args(self):
         """Arguments that aren't --user or :U mounts should be unchanged"""
-        command = [
-            "podman",
-            "run",
-            "--name",
-            "foo",
-            "--user",
-            "37083:1000",
-            "-v",
-            "/path/to/repo:/home/user/mounted_home:U",
-            "--workdir",
-            "/home/user/mounted_home/doc",
-            "-t",
-            "--rm",
-            "-e",
-            "current_version=None",
-            "ghcr.io/escomp/ctsm/ctsm-docs:v1.0.1",
-            "make",
-            "html",
-        ]
-        result = _fix_command_for_missing_subids(command)
-        expected = [
-            "podman",
-            "run",
-            "--name",
-            "foo",
-            "--user",
-            "0:0",
-            "-v",
-            "/path/to/repo:/home/user/mounted_home",
-            "--workdir",
-            "/home/user/mounted_home/doc",
-            "-t",
-            "--rm",
-            "-e",
-            "current_version=None",
-            "ghcr.io/escomp/ctsm/ctsm-docs:v1.0.1",
-            "make",
-            "html",
-        ]
-        self.assertEqual(expected, result)
+        self.assertEqual(_FIXED_COMMAND, self.result)
 
-    def test_no_U_suffix_unchanged(self):
+    def test_no_U_suffix_only_changes_user(self):
         """A command without :U mount should only change --user"""
         command = [
             "podman",
             "run",
             "--user",
-            "37083:1000",
+            _BASE_USER_STR,
             "--mount",
             "type=bind,source=/path,target=/home/user/mounted_home",
             "image:latest",
@@ -117,7 +81,7 @@ class TestFixCommandForMissingSubids(unittest.TestCase):
             "html",
         ]
         result = _fix_command_for_missing_subids(command)
-        self.assertEqual(result[result.index("--user") + 1], "0:0")
+        self.assertEqual(result[result.index("--user") + 1], _FIXED_USER_STR)
         self.assertIn("type=bind,source=/path,target=/home/user/mounted_home", result)
 
 
@@ -130,25 +94,6 @@ def _make_options():
         versions=False,
         build_in_container=True,
     )
-
-
-_BASE_COMMAND = [
-    "podman",
-    "run",
-    "--name",
-    "foo",
-    "--user",
-    "37083:1000",
-    "-v",
-    "/path/to/repo:/home/user/mounted_home:U",
-    "--workdir",
-    "/home/user/mounted_home/doc",
-    "-t",
-    "--rm",
-    "ghcr.io/escomp/ctsm/ctsm-docs:v1.0.1",
-    "make",
-    "html",
-]
 
 
 @patch("doc_builder.build_docs.start_container_software")
@@ -177,7 +122,7 @@ class TestRunBuildCommandRetry(unittest.TestCase):
         mock_check_call.assert_called_once()
         retry_cmd = mock_check_call.call_args[0][0]
         user_idx = retry_cmd.index("--user")
-        self.assertEqual(retry_cmd[user_idx + 1], "0:0")
+        self.assertEqual(retry_cmd[user_idx + 1], _FIXED_USER_STR)
         self.assertNotIn("/path/to/repo:/home/user/mounted_home:U", retry_cmd)
         self.assertIn("/path/to/repo:/home/user/mounted_home", retry_cmd)
 
