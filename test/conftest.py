5d2417619f90230adb251e882ad6ceb6d7878a9c
Sam Rabin <samrabin@ucar.edu>
2026-06-19 13:12:52 -0600

    Delete test/conf.py.

--- test/conftest.py
+++ test/conftest.py
@@ -9,4 +9,4 @@ import pytest
 @pytest.fixture(name="conf_py_path")
 def fixture_conf_py_path():
     """Default absolute path to conf.py file"""
-    return os.path.join(os.path.dirname(__file__), "conf.py")
+    return os.path.join(os.path.dirname(__file__), os.pardir, "conf.py")
